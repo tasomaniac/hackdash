86f5141e21757143f73747d4201a33f5a313e262
Said Tahsin Dane <tasomaniac@gmail.com>
2014-03-26 00:58:02 +0200

    Used ArrayAdapter<HackerSpace> instead of simple String lists to support sorting the list.

--- dashclock/src/main/java/org/istanbulhs/dashclock/ChooseHackerSpaceActivity.java
+++ dashclock/src/main/java/org/istanbulhs/dashclock/ChooseHackerSpaceActivity.java
@@ -5,8 +5,10 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -19,6 +21,8 @@ import com.google.android.apps.dashclock.api.DashClockExtension;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
 
@@ -28,9 +32,8 @@ public class ChooseHackerSpaceActivity extends Activity implements DialogInterfa
 
     private RequestQueue queue;
 
-    private ArrayList<String> spaces;
-    private ArrayList<String> urls;
-    private String choosen_space;
+    private ArrayList<HackerSpace> spaces;
+    private HackerSpace choosen_space;
     private int choosen_index;
 
     @Override
@@ -39,16 +42,19 @@ public class ChooseHackerSpaceActivity extends Activity implements DialogInterfa
 
 
         if(pd == null) {
-            pd = ProgressDialog.show(this, null, "Please wait!", true, true, new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
-                    finish();
-                    queue.cancelAll(ChooseHackerSpaceActivity.this);
-                }
-            });
+            try {
+                pd = ProgressDialog.show(this, null, "Please wait!", true, true, new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        finish();
+                        queue.cancelAll(ChooseHackerSpaceActivity.this);
+                    }
+                });
+            } catch (Exception e){}
         }
 
-        choosen_space = PreferenceManager.getDefaultSharedPreferences(this).getString("space_name", "");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        choosen_space = new HackerSpace(prefs.getString("space_name", ""), prefs.getString("space_url", ""));
         queue = Volley.newRequestQueue(this);
 
         JsonObjectRequest request = new JsonObjectRequest("http://spaceapi.net/directory.json?api=0.13", null,
@@ -59,26 +65,28 @@ public class ChooseHackerSpaceActivity extends Activity implements DialogInterfa
                         if(jsonObject == null) return;
 
                         spaces = new ArrayList<>();
-                        urls = new ArrayList<>();
 
                         Iterator<String> iterator = jsonObject.keys();
                         while(iterator.hasNext()) {
                             String key = iterator.next();
                             String value = jsonObject.optString(key);
                             if(value != null) {
-                                spaces.add(key);
-                                urls.add(value);
+                                spaces.add(new HackerSpace(key, value));
                             }
                         }
+                        Collections.sort(spaces, new Comparator<HackerSpace>() {
+                            @Override
+                            public int compare(HackerSpace lhs, HackerSpace rhs) {
+                                return lhs.toString().compareTo(rhs.toString());
+                            }
+                        });
 
-                        int checkedItem = spaces.indexOf(choosen_space);
-                        if(checkedItem < 0 || checkedItem >= spaces.size())
-                            checkedItem = 0;
+                        choosen_index = spaces.indexOf(choosen_space);
 
                         try {
                             new AlertDialog.Builder(ChooseHackerSpaceActivity.this)
                                     .setTitle(R.string.settings_choose_title)
-                                    .setSingleChoiceItems(spaces.toArray(new String[spaces.size()]), checkedItem, ChooseHackerSpaceActivity.this)
+                                    .setSingleChoiceItems(new ArrayAdapter<>(ChooseHackerSpaceActivity.this, android.R.layout.select_dialog_singlechoice, spaces), choosen_index, ChooseHackerSpaceActivity.this)
                                     .setPositiveButton(android.R.string.ok, ChooseHackerSpaceActivity.this)
                                     .setNegativeButton(android.R.string.cancel, ChooseHackerSpaceActivity.this)
                                     .setCancelable(true)
@@ -116,10 +124,11 @@ public class ChooseHackerSpaceActivity extends Activity implements DialogInterfa
             choosen_index = which;
         }
         else if(which == DialogInterface.BUTTON_POSITIVE) {
+            HackerSpace chosen_space = choosen_index > 0 ? spaces.get(choosen_index) : null;
             PreferenceManager.getDefaultSharedPreferences(this)
                     .edit()
-                    .putString("space_name", spaces.get(choosen_index))
-                    .putString("space_url", urls.get(choosen_index))
+                    .putString("space_name", chosen_space != null ? chosen_space.space : null)
+                    .putString("space_url", chosen_space != null ? chosen_space.url : null)
                     .apply();
             sendBroadcast(new Intent(StatusService.SETTINGS_CHANGED_EVENT));
 
@@ -133,4 +142,5 @@ public class ChooseHackerSpaceActivity extends Activity implements DialogInterfa
     public void onCancel(DialogInterface dialog) {
         finish();
     }
+
 }
